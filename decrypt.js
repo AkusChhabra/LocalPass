02655c7aa961f399416e3300cfb2f0d9846ca2cf
Akus Chhabra <akuschhabra@gmail.com>
2025-05-04 18:45:22 -0400

    Current progress of file encryption and decryption, need to fix bug with iv

--- decrypt.js
+++ decrypt.js
@@ -1,5 +1,7 @@
 const crypto = require("crypto");
 const fs = require("fs");
+const stream  = require("stream");
+const { buffer } = require("stream/consumers");
 //const zlib = require("zlib");
 
 const direc = "./files"
@@ -7,22 +9,42 @@ const direc = "./files"
 //var key = '14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd';
 var secret = "myPass";
 //const key = crypto.createHash('sha256').update(String(secret)).digest('base64').substring(0, 32);
-const key = crypto.scryptSync(secret, 'salt', 32);
+//const key_salt = crypto.scryptSync(secret, 'salt', 32);
 
 
-//const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
-var decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv)
-//var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-var input = fs.createReadStream(direc + '/encrypted.txt');
-//const gzipStream = zlib.createGzip();
-var output = fs.createWriteStream(direc + '/decrypted.txt');
+function decrypt(plaintext) {
 
-input.pipe(decipher).pipe(output);
+    const key = getCipherKey(plaintext);
+    let input = fs.createReadStream(direc + '/encrypted.enc', { start: 0 });
+    let iv = Buffer.alloc(16);
+    let file_inp = fs.readFileSync(direc + '/encrypted.enc');
+    //console.log(Buffer.from(file_inp,'base64').slice(16));
+    iv = Buffer.from(file_inp, 'base64').slice(0,16);
+    //const iv = crypto.randomBytes(16);
+    console.log(iv)
+    console.log(iv.toString())
+    //let input = new stream(Buffer.from(file_inp,'base64').slice(16)); 
+    
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    //let decrypted = Buffer.concat([iv, decipher.update(plaintext)]);
+    //var decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv)
+    //console.log(input)
+    //fs.readSync(fs.openSync(direc + '/encrypted.enc', 'r'), iv, 0, 16, 0);
+    let output = fs.createWriteStream(direc + '/decrypted.enc');
+    //const gzipStream = zlib.createGzip();
 
-output.on('finish', function() {
-  console.log('Decrypted file written to disk!');
-});
-output.on('error', (err) => {
-    console.error('Error during decryption:', err);
-});
+    input.pipe(decipher).pipe(output);
+
+    output.on('finish', function() {
+    console.log('Decrypted file written to disk!');
+    });
+    output.on('error', (err) => {
+        console.error('Error during decryption:', err);
+    });
+}
+
+function getCipherKey(password) {
+  return crypto.createHash('sha256').update(password).digest();
+}
+
+decrypt(secret);
--- encrypt.js
+++ encrypt.js
@@ -1,24 +1,39 @@
 const crypto = require("crypto");
 const fs = require("fs");
-//const zlib = require("zlib");
+const zlib = require("zlib");
 
-const direc = "./files"
+const direc = "./files";
 
 //var key = '14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd';
 const secret = "myPass";
-//const key = crypto.createHash('sha256').update(String(secret)).digest('base64').substring(0, 32);
-const key = crypto.scryptSync(secret, 'salt', 32);
+//const key_salt = crypto.scryptSync(secret, 'salt', 32);
 
-//const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
-var cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv)
-//var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-var input = fs.createReadStream(direc + '/test.txt'); // "try_to_decode_this"
-//const gzipStream = zlib.createGzip();
-var output = fs.createWriteStream(direc + '/encrypted.txt');
+function encrypt(plaintext) {
+  const iv = crypto.randomBytes(16);
+  const key = getCipherKey(plaintext);
+  //const key = crypto.createHash('sha256').update(String(plaintext)).digest('base64').substring(0, 32);
+  let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  //let encrypted = Buffer.concat([iv]);
+  //let encrypted = Buffer.concat([iv, cipher.update(plaintext,'utf-8'), cipher.final()]);
+  //let data = encrypted.toString('base64url');
+  let input = fs.createReadStream(direc + '/test.txt'); // "try_to_decode_this"
+  let output = fs.createWriteStream(direc + '/encrypted.enc');
+  //const gzipStream = zlib.createGzip();
+  
+  console.log(iv);
+  console.log(iv.toString());
+  
+  output.write(iv);
 
-input.pipe(cipher).pipe(output);
+  input.pipe(cipher).pipe(output)
 
-output.on('finish', function() {
-  console.log('Encrypted file written to disk!');
-});
+  output.on('finish', function() {
+    console.log('Encrypted file written to disk!');
+  });
+}
+
+function getCipherKey(password) {
+  return crypto.createHash('sha256').update(password).digest();
+}
+
+encrypt(secret);
