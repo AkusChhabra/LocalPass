f6a012482b13c4192c2934996d1ad67b3b7b2ba9
Akus Chhabra <akuschhabra@gmail.com>
2026-07-12 10:14:01 -0400

    Fixed radiobox not hiding password

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ Helper script to handle application user interactions
 
 */
 
+function openPassWindow() {
+    newPassWindow = window.open('add_password.html', 'Add New Password');
+}
+
 function addRow() {
 
     website = document.getElementById("website-input")
@@ -17,27 +21,22 @@ function addRow() {
     const newRowHTML = `<tr><td>${website}</td><td>${username}</td><td>${pw}</td><td>${last_updated}</td></tr>`;
     table.insertAdjacentHTML("beforeend", newRowHTML);
 
-    window.close('add_password.html');
+    newPassWindow.close('add_password.html');
 }
 
 function deleteRow() {
     const table = document.getElementById("password-table");
 
     function deleteThisRow(button) {
-
-    button.closest('tr').remove();
-}
+        button.closest('tr').remove();
+    }
 }
 
 function passwordVisibility() {
-  var x = document.getElementById("pw");
+  var x = document.getElementById("pw-input");
   if (x.type === "password") {
     x.type = "text";
   } else {
     x.type = "password";
   }
 }
-
-document.getElementById('add-pass-btn').addEventListener('click', () => {
-    window.open('add_password.html', 'Add New Password');
-});
